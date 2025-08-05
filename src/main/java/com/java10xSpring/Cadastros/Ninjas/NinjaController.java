6a36daec807f4afdae1722f1b79cf59ced1c636c
Gustavo <gustavo.borba.27@gmail.com>
2025-08-05 18:40:55 -0300

    Modelando banco ainda somente as bases.

--- src/main/java/com/java10xSpring/Cadastros/Ninjas/NinjaController.java
+++ src/main/java/com/java10xSpring/Cadastros/Ninjas/NinjaController.java
@@ -1,4 +1,4 @@
-package com.java10xSpring.Cadastros;
+package com.java10xSpring.Cadastros.Ninjas.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
